1e66ff5591c654712cd5d8265660f9c820b34f09
DAFTDESAILLY <desita_gsheko@hotmail.com>
2023-07-25 23:54:23 -0600

    agregando registro

---
+++ app/src/main/java/com/Desailly/kotlindelivery/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.Desailly.kotlindelivery.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.Desailly.kotlindelivery.R
+
+class MainActivity : AppCompatActivity() {
+
+    var imageViewGoToRegister : ImageView?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageViewGoToRegister = findViewById(R.id.imageview_go_to_register)
+
+        imageViewGoToRegister?.setOnClickListener{goToRegister()}
+    }
+    private fun goToRegister(){
+        val i = Intent(this,RegisterActivity::class.java)
+        startActivity(i)
+    }
+
+}
--- app/src/main/java/com/Desailly/kotlindelivery/activities/RegisterActivity.kt
+++ app/src/main/java/com/Desailly/kotlindelivery/activities/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.Desailly.kotlindelivery
+package com.Desailly.kotlindelivery.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.Desailly.kotlindelivery.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
